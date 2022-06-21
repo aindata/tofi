c0b80f9fde2f588394087c0085d6151344f4fe57
kirnap <okirnap@gmail.com>
2022-06-21 14:56:07 +0100

    initial commit

---
+++ src/tofi/sampling/uncertainty_sampling.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def least_confidence(proba: np.ndarary, issorted:bool = False) -> np.ndarary:
+    """
+
+    :param proba: Prediction probabilities of an instance
+    :param issorted: checks whether proba is sorted
+    :return: sampling score of prediction
+    """
+
+    pass
+
+
+def margin_confidence():
+    pass
+
+
+def ratio_confidence():
+    pass
+
+
+def entropy_based_confidence():
+    pass
+
