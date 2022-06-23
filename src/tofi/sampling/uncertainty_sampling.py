7a8f1361f996d104ae176f1634a71918f6f67f25
kirnap <okirnap@gmail.com>
2022-06-23 10:47:22 +0100

    implement uncertainty sampling

--- src/tofi/sampling/uncertainty_sampling.py
+++ src/tofi/sampling/uncertainty_sampling.py
@@ -1,25 +1,66 @@
 import numpy as np
 
 
-def least_confidence(proba: np.ndarary, issorted:bool = False) -> np.ndarary:
+def least_confidence(proba: np.ndarray, issorted: bool = False) -> np.ndarray:
     """
 
-    :param proba: Prediction probabilities of an instance
+    :param proba: Prediction probabilities of an instance has a dimension of 1 by number of classes
     :param issorted: checks whether proba is sorted
     :return: sampling score of prediction
+
+    TODO: write latex formula for the implementation
     """
 
-    pass
+    if issorted:
+        confidence = proba[0]
+    else:
+        confidence = np.max(proba)
+    num_labels = proba.size
+
+    normalized_confidence = (1 - confidence) * (num_labels / (num_labels - 1))
+    return normalized_confidence
 
 
-def margin_confidence():
-    pass
+def margin_confidence(proba: np.ndarray, issorted: bool = False) -> np.ndarray:
+    """
+
+    :param proba: :param proba: Prediction probabilities of an instance has a dimension of 1 by number of classes
+    :param issorted:
+    :return:
+    """
+    sorted_proba = proba
+    if not issorted:
+        sorted_proba = np.sort(proba)[::-1]  # sort probs s.t. the largest is the first
+
+    diff = (sorted_proba[0] - sorted_proba[1])  # difference between top two probabilities
+    # TODO: why subtracting the top 2 probabilities?
+
+    margin_conf = 1 - diff
+    return margin_conf
 
 
-def ratio_confidence():
-    pass
+def ratio_confidence(proba: np.ndarray, issorted: bool = False) -> np.ndarray:
+    """
+
+    :param proba: :param proba: Prediction probabilities of an instance has a dimension of 1 by number of classes
+    :param issorted:
+    :return:
+    """
+    sorted_proba = proba
+    if not issorted:
+        sorted_proba = np.sort(proba)[::-1]  # sort probs s.t. the largest is the first
+    ratio = sorted_proba[1] / sorted_proba[0]  # ratio between top two pros
+    # TODO: why dividing the top two probabilities? what if there are more than 2 classes?
+    return ratio
 
 
-def entropy_based_confidence():
-    pass
+def entropy_confidence(proba: np.ndarray) -> np.ndarray:
+    """
 
+    :param proba: :param proba: Prediction probabilities of an instance has a dimension of 1 by number of classes
+    :return:
+    """
+    log_probs = proba * np.log2(proba)
+    raw_entropy = 0 - np.sum(log_probs)
+    normalized_entropy = raw_entropy / np.log2(proba.size)
+    return normalized_entropy
--- tests/sampling/test_uncertainty_sampling.py
+++ tests/sampling/test_uncertainty_sampling.py
@@ -5,52 +5,68 @@ https://github.com/rmunro/pytorch_active_learning/blob/master/uncertainty_sampli
 We expect the same output from our functions as with Robert Munro's repo
 
 Please note that methods from Robert Munro's repo is denoted with mu_ prefix
-# TODO: validate better testing
+
 """
+import sys
+sys.path.append('../..')  # TODO: find better way to import files from src
+from src.tofi.sampling import uncertainty_sampling
 
 
-def test_least_confidence():
+# TODO: validate better testing
+
+def test_least_confidence(proba):
     """
     A test case scenario for an artificial input:
 
     mu_least_confidence:
-     - input: tensor([[0.8254, 0.1746]])
-     - output: 0.34910380840301514
+     - proba: tensor([0.8305774  0.16942263])
+     - output: 0.33884525299072266
 
     """
-    pass
+    assert np.isclose(uncertainty_sampling.least_confidence(proba), 0.33884525299072266)
 
 
-def test_margin_confidence():
+def test_margin_confidence(proba):
     """
     A test case scenario for an artificial input:
 
     mu_margin_confidence:
-     - input: tensor([0.8254, 0.1746])
-     - output: 0.3491038680076599
+     - proba: tensor([0.8305774  0.16942263])
+     - output: 0.33884525299072266
 
     """
-    pass
+    assert np.isclose(uncertainty_sampling.margin_confidence(proba), 0.33884525299072266)
 
 
-def test_ratio_confidence():
+def test_ratio_confidence(proba):
     """
     A test case scenario for ratio confidence
     mu_ratio_confidence:
-     - input: tensor([0.8254, 0.1746])
-     - output: 0.21146325767040253
+     - proba: tensor([0.8305774  0.16942263])
+     - output: 0.20398175716400146
     """
-    pass
+    assert np.isclose(uncertainty_sampling.ratio_confidence(proba), 0.20398175716400146)
 
 
-def test_entropy_based_confidence():
+
+def test_entropy_confidence(proba):
     """
     A test case scenario for entropy-based confidence
 
     mu_entropy_based:
-     - input: tensor([0.8254, 0.1746])
-     - output: 0.6680124998092651
+     - proba: tensor([0.8305774  0.16942263])
+     - output: 0.6563822627067566
 
     """
-    pass
+    np.isclose(uncertainty_sampling.entropy_confidence(proba), 0.6563822627067566)
 
+
+
+if __name__ == '__main__':
+    import numpy as np
+    test_input = np.array([0.8305774 , 0.16942263], dtype=np.float32)
+    test_least_confidence(test_input)
+    test_margin_confidence(test_input)
+    test_ratio_confidence(test_input)
+    test_entropy_confidence(test_input)
+    print("Uncertainty sampling tests passed!")
